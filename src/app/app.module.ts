44a52f1d2c81aea43d2512793d741ff58dcea90b
Cliffochieng <cliffordochieng844@gmail.com>
2020-09-30 21:03:16 +0300

    add components and service folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GiphyDisplayComponent } from './components/giphy-display/giphy-display.component';
+import { RandomComponent } from './components/random/random.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TrendingComponent } from './components/trending/trending.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GiphyDisplayComponent,
+    RandomComponent,
+    NavbarComponent,
+    TrendingComponent
   ],
   imports: [
     BrowserModule,
