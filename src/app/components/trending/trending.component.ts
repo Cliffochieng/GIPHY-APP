c273100d09d8cf97140ae69e1bc863ae4eb2d2c4
Cliffochieng <cliffordochieng844@gmail.com>
2020-10-01 16:02:26 +0300

    add  a method to call out the trending gifs

--- src/app/components/trending/trending.component.ts
+++ src/app/components/trending/trending.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { GiphyService } from 'src/app/services/giphy.service';
+import { GiphyService } from 'src/app/services/giphy-service';
 @Component({
   selector: 'app-trending',
   templateUrl: './trending.component.html',
   styleUrls: ['./trending.component.css']
 })
 export class TrendingComponent implements OnInit {
-
-  constructor() { }
+  trendingGifs: any;
+  constructor(private service: GiphyService) { }
 
   ngOnInit(): void {
+    this.service.trendingGifs()
+    .subscribe(response => {
+      this.trendingGifs = response['data'];
+    });
   }
 
 }
