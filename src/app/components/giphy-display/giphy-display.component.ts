3dcd5a7eee465bf5f4312539303097aac7441b74
Cliffochieng <cliffordochieng844@gmail.com>
2020-10-01 11:46:56 +0300

    add search logic

--- src/app/components/giphy-display/giphy-display.component.ts
+++ src/app/components/giphy-display/giphy-display.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./giphy-display.component.css']
 })
 export class GiphyDisplayComponent implements OnInit {
+  gifs: any;
+  searchQuery: string;
+  notFound = false;
 
-  constructor() { }
+  constructor(private service: GiphyService) { }
 
   ngOnInit(): void {
   }
-
+     
+  searchGifs() {
+    this.service.searchGifs(this.searchQuery);
+    this.service.getGifs()
+    .subscribe(results => {
+      // tslint:disable-next-line: no-string-literal
+      console.log(results['data']);
+      // tslint:disable-next-line: no-string-literal
+      this.gifs = results['data'];
+    }, error => {
+      alert('not found :(');
+    });
+    this.searchQuery = '';
+  }
 }
