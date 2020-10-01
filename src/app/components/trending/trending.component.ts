7e20172c55f168f2f2874d5ea0b784d8c1ddd611
Cliffochieng <cliffordochieng844@gmail.com>
2020-10-01 15:55:00 +0300

    import giphy-service to trending c.ts

--- src/app/components/trending/trending.component.ts
+++ src/app/components/trending/trending.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { GiphyService } from 'src/app/services/giphy.service';
 @Component({
   selector: 'app-trending',
   templateUrl: './trending.component.html',
